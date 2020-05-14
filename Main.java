64468873347dd14e7a20ef0438149b981a30729c
Redmond Chan <redmondchan92@gmail.com>
2020-05-14 01:23:26 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,49 @@
+class Main {
+  public static void main(String[] args) {
+
+    // ---------------declaring variables----------------------
+    // int y = 1;
+    // String x = "hi";
+    // String output = String.format("%s number %d", x, y);
+    // System.out.println(output);
+
+    // -----------------array------------------------
+
+    // int[] arr1 = {1,2,3,4,5};
+    // String[] arr2 = {"test", "hi"};
+    // String output = String.format("%s number %d", arr2[1], arr1[0]);
+    // System.out.println(output);
+
+    // -----------------multidimensional array------------------------
+
+    // int[][] arr1 = { {6,7,8,9}, {1,2,3,4,5} };
+    // String[][] arr2 = { {"test","testing"}, {"java","array","multi"}, {"hey", "hello", "bye", "hi"} };
+    // String output = String.format("%s number %d", arr2[2][3], arr1[1][0]);
+    // System.out.println(output);
+    // // System.out.println(arr2.length);
+
+    // -----------------conditionals------------------------
+    // // int age = 9;
+    // // int age = 16;
+    // int age = 21;
+
+    // if (age < 13){
+    //   System.out.println("You are too young to be a teenager");
+    // } else if (12 < age && age < 20){
+    //   System.out.println("You are a teenager");
+    // } else {
+    //   System.out.println("You are too old to be a teenager");
+    // }
+
+    // -----------------loops------------------------
+
+    // int[] arr = {1, 2, 3, 4, 5};
+
+    // for(int i = 0; i < 5; i++){
+    //   System.out.println(arr[i]);
+    // }
+
+
+    // -----------------classes + objects------------------------
+  }
+}
